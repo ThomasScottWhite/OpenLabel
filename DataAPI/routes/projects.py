061bfd96b5373cab613c0e7d729c6d9c2e9548ab
heitmeih <harry.heitmeier@gmail.com>
2025-04-12 16:35:51 -0500

    add base route files for auth and projects route categories

---
+++ DataAPI/routes/projects.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import logging
+from typing import Final
+
+from fastapi import APIRouter
+
+logger = logging.getLogger(__name__)
+
+_section_name: Final[str] = "projects"
+
+# Do not change the name of "router"!
+router = APIRouter(prefix=f"/{_section_name}", tags=[_section_name])
+
+
+@router.get("/hello")
+def hello() -> str:
+    return "hello, world!"
