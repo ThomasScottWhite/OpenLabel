3d3e81a0b611a055ab9724124f6452ca0bc44e63
heitmeih <harry.heitmeier@gmail.com>
2025-04-28 23:39:30 -0500

    fix config

--- OpenLabelBackend/DataAPI/config.py
+++ OpenLabelBackend/DataAPI/config.py
@@ -1,9 +1,13 @@
 from typing import Final
-import os
+
 from pydantic_settings import BaseSettings
 
-# Modify the environment variable to set the test database
+
 class _Config(BaseSettings):
+    # put app config parameters here
+    # values will be automatically updated from environment variables
+    port: int = 6969
+
     mongo_uri: str = "mongodb://localhost:27017"
     database_name: str = "openlabel_db"
 
