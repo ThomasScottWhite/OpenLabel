feb89ede2330980da299175b9547dbc2bbe38ccc
heitmeih <harry.heitmeier@gmail.com>
2025-05-06 14:21:54 -0500

    clean up old DB manager files

--- OpenLabelBackend/OpenLabelDB/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-# MongoDB connection settings
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "openlabel_db")
